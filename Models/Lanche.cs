28610d814b704ed26998d1ad4e19b6675d128f31
Alisson- Desktop <alisson2001vieira@gmail.com>
2022-03-01 09:34:15 -0300

    Implementação do Banco, usando Code First

---
+++ Models/Lanche.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
+{
+    [Table("Lances")]
+    public class Lanche
+    {
+        [Key]
+        public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "O nome do lanche deve ser preenchido")]
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser preenchida")]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(100, ErrorMessage = "Descrição deve ter no máximo {1} caracteres")]
+        public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "A descrição detalhada do lanche deve ser preenchida")]
+        [Display(Name = "Descrição detalhada do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição deve ter no máximo {1} caracteres")]
+        public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 999.999, ErrorMessage = "O preço deve estar entre R$1,00 e R$999,99.")]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho da imagem normal")]
+        public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho da imagem miniatura")]
+        public string ImagemThumbNailUrl { get; set; }
+
+        [Display(Name = "Preferido?")]
+        public bool LanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaID { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
