000952560ed9356c0578ee071e214f5c9aa2adb0
Alisson- Desktop <alisson2001vieira@gmail.com>
2022-03-02 10:04:45 -0300

    Adição de ViewModels e PartialViews

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+        public IActionResult List()
+        {
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _lancheRepository.Lanches;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lancheListViewModel);
+        }
+    }
+}
